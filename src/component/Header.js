e6f00093cd9f8b5b83341514b4e3d01d94483d00
unknown <maniplek81@gmail.com>
2022-02-10 21:26:52 +0200

    first commit

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <h2>Edit Set</h2>
+        <hr></hr>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/component/content.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function content() {
+  return (
+    <div>
+        <div>
+            <h4>color</h4>
+        </div>
+        <div>
+            <input type='text' placeholder='Outcome name' />
+            <input type='text' placeholder='Description' />
+            <input type='text' placeholder='ID' />
+        </div>
+        <div>
+            
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default content
